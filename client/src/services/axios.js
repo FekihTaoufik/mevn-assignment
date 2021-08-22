bd49cedf8294f40a38a1f7ad2b9053cc16179c8f
Taoufik Fekih <tafekih@outlook.fr>
2021-08-22 15:52:29 +0200

    axios interceptor error handler fix

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import axios from "axios";
 import store from "@/store";
+import router from "@/router";
 import { errorHandler } from "../utils/errorHandler";
 
 let config = {
@@ -9,8 +10,10 @@ let config = {
 
 const _axios = axios.create(config);
 
+//  Intercepting request
 _axios.interceptors.request.use(
   function (config) {
+    // Injecting api if defined
     if (store.getters.isAuthenticated) {
       config.headers.authorization = "Bearer " + store.state.token;
     }
@@ -22,17 +25,18 @@ _axios.interceptors.request.use(
   }
 );
 
-// Add a response interceptor
+// Intercepting response
 _axios.interceptors.response.use(
   function (response) {
-    // Do something with response data
     return response;
   },
   async function (error) {
+    // Handling errors and showing them in a toast message
     errorHandler(Vue, error);
     if (error.response.status == 401) {
       store.dispatch("unAuthenticate");
-      if (window.location.pathname !== "/") window.location = "/";
+      if (window.location.pathname !== router.options.base)
+        window.location = router.options.base;
     }
 
     return Promise.reject(error);
